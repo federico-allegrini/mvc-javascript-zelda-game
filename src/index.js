d69fafd2d7e1ef0cde59f59bbcfcb29c7097d641
federico-allegrini-mc <federicoallegrini@mondoconv.it>
2022-10-26 15:15:08 +0200

    Basic webpack configuation

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import "./assets/css/style.css";
+
+const title = document.createElement("h2");
+title.textContent = "Zelda Game";
+
+const page = document.querySelector("body");
+page.append(title);
