b40f92a09bff77f7c789435b336b8956a61f59ed
Federico Allegrini <federico.allegrini.dev@gmail.com>
2022-10-30 18:21:51 +0100

    Fixed allowed values function for working with objects

--- src/lib/checkAllowedValues.js
+++ src/lib/checkAllowedValues.js
@@ -1,11 +1,12 @@
 export function checkAllowedValues(value, allowedValues) {
   const valueUpperCase = value.toUpperCase().trim();
-  if (allowedValues.includes(valueUpperCase)) {
+  const allowedValuesList = Object.values(allowedValues);
+  if (allowedValuesList.includes(valueUpperCase)) {
     return { allowed: true, value: valueUpperCase };
   } else {
     return {
       allowed: false,
-      value: `Allowed values are only: ${this.allowedValues
+      value: `Allowed values are only: ${allowedValuesList
         .join(", ")
         .slice(0, -2)}!`,
     };
