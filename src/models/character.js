8aa64812936836856e27f5267991190e11487dad
Federico Allegrini <federico.allegrini.dev@gmail.com>
2022-10-30 18:21:13 +0100

    Fixed character and room classes

--- src/models/character.js
+++ src/models/character.js
@@ -8,7 +8,7 @@ class Character {
   // Weapon that kills him
   object;
 
-  constructor(name, object) {
+  constructor(name, object = undefined) {
     this.name = name.toUpperCase().trim();
     this.object = object;
     this.endGame = this.name === CHARACTER_TYPES.princess;
--- src/models/room.js
+++ src/models/room.js
@@ -11,14 +11,16 @@ class Room {
   constructor(number, description, walls, objects) {
     this.number = number;
     this.description = description;
-    if (walls?.length === 4) {
-      if (this.isBlockedMaximumOne(walls)) {
-        this.walls = walls;
+    if (walls) {
+      if (walls.length === 4) {
+        if (this.isBlockedMaximumOne(walls)) {
+          this.walls = walls;
+        } else {
+          throw `You cannot create a room with multiple blocked walls!`;
+        }
       } else {
-        throw `You cannot create a room with multiple blocked walls!`;
+        throw `You cannot create a room with a number of walls other than 4!`;
       }
-    } else {
-      throw `You cannot create a room with a number of walls other than 4!`;
     }
     this.objects = objects;
   }
